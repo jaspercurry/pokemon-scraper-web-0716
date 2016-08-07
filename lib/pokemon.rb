488e52eb7ffb95a8a647d91127b50002eaee53e3
jaspercurry <jaspercurry@gmail.com>
2016-08-07 14:19:15 -0400

    made pokemon class

--- lib/pokemon.rb
+++ lib/pokemon.rb
@@ -1,2 +1,24 @@
 class Pokemon
+
+  attr_accessor :id, :name, :type, :db
+  def initialize(id = nil, name, type, db)
+    @id = id
+    @name = name
+    @type = type
+    @db = db
+  end
+
+  def self.save(name, type, db)
+    db.execute("INSERT INTO pokemon (name, type) VALUES (?, ?)", name, type)
+  end
+
+  def self.find(id, db)
+    db.execute("SELECT id, name, type FROM pokemon WHERE id = (?)", id)
+  end
+
+  def alter_hp(hp)
+    db.execute("UPDATE pokemon SET hp = ? WHERE name = (?)", hp, self.name)
+  end
+
+
 end
